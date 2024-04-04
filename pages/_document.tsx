9da2899f5ed132e6c7a130979dab63c2fa505b76
Lim dong hyeon <ilhai3@naver.com>
2024-04-04 19:15:20 +0900

    배포 오류 수정5

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,10 @@ export default function Document() {
     <Html lang="kr">
       <Head>
         <script
-          defer
           src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.1/kakao.min.js"
-        />
+          integrity="sha384-kDljxUXHaJ9xAb2AzRd59KxjrFjzHa5TAoFQ6GbYTCAG0bjM55XohjjDT7tDDC01"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <body>
         <Main />
